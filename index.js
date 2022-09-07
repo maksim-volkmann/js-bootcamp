6053e8161f74afe6d9e47b1637eb311bc038b411
mustafadennis <mustafa.ozdemir@pipedrive.com>
2022-09-07 18:11:16 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userModel from "./userModel.js";
+
+const app = express();
+const port = 3001;
+
+dotenv.config();
+
+app.use(express.json());
+
+const connectionToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connection to mongoDB is successfull!");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+app.post("/create", async (req, res) => {
+  try {
+    const newUser = new userModel(req.body);
+    await newUser.save();
+    res.status(201).send("New user is created");
+  } catch (error) {
+    res.status(405).send(error);
+    console.error(error);
+  }
+});
+
+app.listen(port, () => {
+  connectionToDB();
+  console.log(`Server started on port ${port}`);
+});
